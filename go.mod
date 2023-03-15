ff51df40f141a37cebb5a1add0ca942623be2c80
Gabriel Bonin <gabrielbonin@MacBook-Pro-de-Gabriel.local>
2023-03-14 23:01:44 -0300

    improve models and request API openai

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gabrielbonin/whatsAppGpt
+
+go 1.20
---
+++ whatsAppGPT/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type Request struct {
+	Model     string    `json:"model"`
+	Messages  []Message `json:"messages"`
+	MaxTokens int       `json:"max_tokens,omitempty"`
+}
+
+type Response struct {
+	ID      string   `json:"id"`
+	Object  string   `json:"object"`
+	Created int      `json:"created"`
+	Choices []Choice `json:"choices"`
+}
+
+type Choice struct {
+	Index   int `json:"index"`
+	Message struct {
+		Role    string `json:"role"`
+		Content string `json:"content"`
+	} `json:"message"`
+}
+
+func GenerateGPTText(query string) (string, error) {
+	req := Request{
+		Model: "gpt-3.5-turbo",
+		Messages: []Message{
+			{
+				Role:    "user",
+				Content: query,
+			},
+		},
+		MaxTokens: 50,
+	}
+	reqJson, err := json.Marshal(req)
+	if err != nil {
+		return "", err
+	}
+	request, err := http.NewRequest("POST", "https://api.opnai.com/v1/chat/completions", bytes.NewBuffer(reqJson))
+	if err != nil {
+		return "", err
+	}
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Authorization", "Bearer YOUR_API_KEY")
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+	var resp Response
+	err = json.Unmarshal(responseBody, &resp)
+	if err != nil {
+		return "", err
+	}
+	return resp.Choices[0].Message.Content, nil
+}
